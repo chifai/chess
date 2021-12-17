4bb3305c5906c11528366c9254faca603608a288
chifai <cfw.chifai@gmail.com>
2021-12-18 06:30:39 +0800

    finish pawn basic move

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -1,5 +1,6 @@
 #include "Pawn.h"
 #include <cassert>
+#include <cmath>
 
 CPawn::CPawn(const ETeam Team) : CPiece(Team)
 {
@@ -18,8 +19,23 @@ EType CPawn::getType() const
 bool CPawn::isValidMove(const TPosition &From, const TPosition &To) const
 {
     // check type
-    // EType Type = m_pPiecePos[From.x][From.y].getType();
-    // assert( m_pPiecePos[From.x][From.y].getType() == Chess::Pawn);
+    const CPiece *pFrom = m_pPieceMap->at(From);
+    const CPiece *pTo = m_pPieceMap->at(To);
+    EType Type = pFrom->getType();
+    assert( Type == Chess::Pawn);
 
+    // check step
+    if (From.x + m_nRowToMoveEach != To.x) return false;
+
+    if (From.y == To.y) {
+        // can only move forward to empty piece
+        return pTo == nullptr;
+    }
+    else if (From.y - To.y == 1 || From.y - To.y == -1) {
+        // can only take opponents pawn when moving diagonally
+        return pTo != nullptr && pTo->getTeam() != m_Team;
+    }
+
+    // #TODO: check en passant
     return false;
 }
