a05ed5f2e1721d2be07d3eb87020b317ae856619
chifai <cfw.chifai@gmail.com>
2021-12-18 06:46:31 +0800

    improve: comments, null protection

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -21,8 +21,9 @@ bool CPawn::isValidMove(const TPosition &From, const TPosition &To) const
     // check type
     const CPiece *pFrom = m_pPieceMap->at(From);
     const CPiece *pTo = m_pPieceMap->at(To);
+    assert(pFrom != nullptr);
     EType Type = pFrom->getType();
-    assert( Type == Chess::Pawn);
+    assert(Type == Chess::Pawn);
 
     // check step
     if (From.x + m_nRowToMoveEach != To.x) return false;
@@ -32,7 +33,7 @@ bool CPawn::isValidMove(const TPosition &From, const TPosition &To) const
         return pTo == nullptr;
     }
     else if (From.y - To.y == 1 || From.y - To.y == -1) {
-        // can only take opponents pawn when moving diagonally
+        // can only move diagonally when opponent's piece is there
         return pTo != nullptr && pTo->getTeam() != m_Team;
     }
 
